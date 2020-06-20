a3a1863c1c26f93855a6998cf3576a1090ff13d0
@sofiaVP <sofia.sarango@gmail.com>
2020-06-20 21:47:26 +0200

    creat vpc with data

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+/*provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+  generation = 1
+  region = var.region
+}
 resource "ibm_is_vpc" "vpc1" {
   name = "vpc1"
+}*/
+data "ibm_is_vpc" "vpc1" {
+  name = var.vpc_name
+}
+data "ibm_resource_group" "resource_group" {
+  name = var.resource_group
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_name" {
+  description = "Name for vpc"
+  type        = string
+  default     = "test"
+}
+
+variable "resource_group" {
+  description = "Group for default"
+  type        = string
+  default     = "Default"
+}
